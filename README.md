f85321332dd33c65f64a1840c49519d97114fc2a
Fabian Beiner <fb@fabianbeiner.de>
2017-12-26 15:06:14 +0100

    Rewrite. :)

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# PHP Dummy Image Generator
-## as seen on DummyImage.com
+# Dynamic Dummy Image Generator — as seen on DummyImage.com
 
-### Examples:
- * image.php?size=250x850&type=jpg&bg=ff8800&color=000000 <br>- will create a 250px width, 800px height jpg image with orange background and black text
- * image.php?size=250 <br>- will create a 250px width, 250px height png image with black background and white text
+**This script enables you to create placeholder images in a breeze.**
+
+## Usage
+
+This script handles the following parameters, where basically all of them are optional.
+
+* `size` — Examples:<br>500x250 (= 500px width, 250px height)<br>500 (= 500px width, 500px height)
+* `type` — Examples:<br>png (= PNG image)<br>gif (= GIF image)<br>jpeg or jpg (= JPEG image)
+* `bg` — Examples:<br>f00 (= #FF0000 as background color)<br>FF0855 (= #FF0855 as background color)
+* `color` — Examples:<br>000 (= #000000 as font color)<br>FFFFFF (= #FFFFFF as font color)
+* `text` — Examples:<br>Lore Ipsum (= Image has Lore Ipsum written on it)
+
+### Credits
+Original idea by [Russel Heimlich](https://github.com/kingkool68/). When I first published this script, [DummyImage.com](https://dummyimage.com/) was not Open Source, so I had to write a small script to replace the function on my own server.
--- image.php
+++ image.php
@@ -1,112 +1,155 @@
 <?php
 /**
- * Dynamic Dummy Image Generator - as seen on DummyImage.com
+ * Dynamic Dummy Image Generator — as seen on DummyImage.com
  *
- * You can create dummy images with this script kinda easily. Just provide at least the size parameter, that's it.
- * Examples:
+ * This script enables you to create placeholder images in a breeze.
+ * Please refer to the README on how to use it.
  *
- * image.php?size=250x850&type=jpg&bg=ff8800&color=000000
- * - will create a 250px width, 800px height jpg image with orange background and black text
+ * (Original idea by Russel Heimlich. When I first published this script,
+ * DummyImage.com was not Open Source, so I had to write a small script to
+ * replace the function on my own server.)
  *
- * image.php?size=250
- * - will create a 250px width, 250px height png image with black background and white text
- *
- * Original idea and script by Russel Heimlich (see http://DummyImage.com). Rewritten by Fabian Beiner.
- *
- * @author Russell Heimlich
- * @author Fabian Beiner <mail -at- fabian-beiner -dot- de)
+ * @author  Fabian Beiner <fb@fabianbeiner.de>
+ * @license MIT
+ * @link    https://github.com/FabianBeiner/PHP-Dummy-Image-Generator/
+ * @version 0.3.0 <2017-12-26>
  */
 
-// Handle the parameters.
-$strSize  = (($strSize = $_GET['size'])   ? strtolower($strSize)  : NULL);
-$strType  = (($strType = $_GET['type'])   ? strtolower($strType)  : 'png');
-$strBg    = (($strBg = $_GET['bg'])       ? strtolower($strBg)    : '000000');
-$strColor = (($strColor = $_GET['color']) ? strtolower($strColor) : 'ffffff');
-
-// Now let's check the parameters.
-if ($strSize == NULL) {
-	die('<b>You have to provide the size of the image.</b> Example: 250x320.</b>');
+/**
+ * Handle the “size” parameter.
+ */
+$size = '640x480';
+if (isset($_GET['size'])) {
+    $size = $_GET['size'];
 }
-if ($strType != 'png' and $strType != 'gif' and $strType != 'jpg') {
-	die('<b>The selected type is wrong. You can chose between PNG, GIF or JPG.');
+list($imgWidth, $imgHeight) = explode('x', $size . 'x');
+if ($imgHeight === '') {
+    $imgHeight = $imgWidth;
 }
-if (strlen($strBg) != 6 and strlen($strBg) != 3) {
-	die('<b>You have to provide the background color as hex.</b> Example: 000000 (for black).');
+$filterOptions = [
+    'options' => [
+        'min_range' => 0,
+        'max_range' => 9999
+    ]
+];
+if (filter_var($imgWidth, FILTER_VALIDATE_INT, $filterOptions) === false) {
+    $imgWidth = '640';
 }
-if (strlen($strColor) != 6 and strlen($strColor) != 3) {
-	die('<b>You have to provide the font color as hex.</b> Example: ffffff (for white).');
+if (filter_var($imgHeight, FILTER_VALIDATE_INT, $filterOptions) === false) {
+    $imgHeight = '480';
 }
 
-// Get width and height from current size.
-list($strWidth, $strHeight) = split('x', $strSize);
-// If no height is given, we'll return a squared picture.
-if ($strHeight == NULL) $strHeight = $strWidth;
-
-// Check if size and height are digits, otherwise stop the script.
-if (ctype_digit($strWidth) and ctype_digit($strHeight)) {
-	// Check if the image dimensions are over 9999 pixel.
-	if (($strWidth > 9999) or ($strHeight > 9999)) {
-		die('<b>The maximum picture size can be 9999x9999px.</b>');
-	}
-
-	// Let's define the font (size. And NEVER go above 9).
-	$intFontSize = $strWidth / 16;
-	if ($intFontSize < 9) $intFontSize = 9;
-	$strFont = "DroidSansMono.ttf";
-	$strText = $strWidth . 'x' . $strHeight;
-
-	// Create the picture.
-	$objImg = @imagecreatetruecolor($strWidth, $strHeight) or die('Sorry, there is a problem with the GD lib.');
-
-	// Color stuff.
-	function html2rgb($strColor) {
-		if (strlen($strColor) == 6) {
-			list($strRed, $strGreen, $strBlue) = array($strColor[0].$strColor[1], $strColor[2].$strColor[3], $strColor[4].$strColor[5]);
-		} elseif (strlen($strColor) == 3) {
-			list($strRed, $strGreen, $strBlue) = array($strColor[0].$strColor[0], $strColor[1].$strColor[1], $strColor[2].$strColor[2]);
-		}
-
-		$strRed   = hexdec($strRed);
-		$strGreen = hexdec($strGreen);
-		$strBlue  = hexdec($strBlue);
-
-		return array($strRed, $strGreen, $strBlue);
-	}
-
-	$strBgRgb    = html2rgb($strBg);
-	$strColorRgb = html2rgb($strColor);
-	$strBg       = imagecolorallocate($objImg, $strBgRgb[0], $strBgRgb[1], $strBgRgb[2]);
-	$strColor    = imagecolorallocate($objImg, $strColorRgb[0], $strColorRgb[1], $strColorRgb[2]);
-
-	// Create the actual image.
-	imagefilledrectangle($objImg, 0, 0, $strWidth, $strHeight, $strBg);
-
-	// Insert the text.
-	$arrTextBox    = imagettfbbox($intFontSize, 0, $strFont, $strText);
-	$strTextWidth  = $arrTextBox[4] - $arrTextBox[1];
-	$strTextHeight = abs($arrTextBox[7]) + abs($arrTextBox[1]);
-	$strTextX      = ($strWidth - $strTextWidth) / 2;
-	$strTextY      = ($strHeight - $strTextHeight) / 2 + $strTextHeight;
-	imagettftext($objImg, $intFontSize, 0, $strTextX, $strTextY, $strColor, $strFont, $strText);
-
-	// Give out the requested type.
-	switch ($strType) {
-		case 'png':
-			header('Content-Type: image/png');
-			imagepng($objImg);
-			break;
-		case 'gif':
-			header('Content-Type: image/gif');
-			imagegif($objImg);
-			break;
-		case 'jpg':
-			header('Content-Type: image/jpeg');
-			imagejpeg($objImg);
-			break;
-	}
-
-	// Free some memory.
-	imagedestroy($objImg);
-} else {
-	die('<b>You have to provide the size of the image.</b> Example: 250x320.</b>');
+/**
+ * Handle the “type” parameter.
+ */
+$type = 'png';
+if (isset($_GET['type']) && in_array(strtolower($_GET['type']), ['png', 'gif', 'jpg', 'jpeg'])) {
+    $type = strtolower($_GET['type']);
 }
+
+/**
+ * Handle the “text” parameter.
+ */
+$text = $imgWidth . '×' . $imgHeight;
+if (isset($_GET['text']) && strlen($_GET['text'])) {
+    $text = filter_var(trim($_GET['text']), FILTER_SANITIZE_STRING);
+}
+$encoding = mb_detect_encoding($text, 'UTF-8, ISO-8859-1');
+if ($encoding !== 'UTF-8') {
+    $text = mb_convert_encoding($text, 'UTF-8', $encoding);
+}
+$text = mb_encode_numericentity($text,
+                                [0x0, 0xffff, 0, 0xffff],
+                                'UTF-8');
+
+/**
+ * Handle the “bg” parameter.
+ */
+$bg = 'CC0099';
+if (isset($_GET['bg']) && (strlen($_GET['bg']) === 6 || strlen($_GET['bg']) === 3)) {
+    $bg = strtoupper($_GET['bg']);
+    if (strlen($_GET['bg']) === 3) {
+        $bg =
+            strtoupper($_GET['bg'][0] .
+                       $_GET['bg'][0] .
+                       $_GET['bg'][1] .
+                       $_GET['bg'][1] .
+                       $_GET['bg'][2] .
+                       $_GET['bg'][2]);
+    }
+}
+list($bgRed, $bgGreen, $bgBlue) = sscanf($bg, "%02x%02x%02x");
+
+/**
+ * Handle the “color” parameter.
+ */
+$color = 'FFFFFF';
+if (isset($_GET['color']) && (strlen($_GET['color']) === 6 || strlen($_GET['color']) === 3)) {
+    $color = strtoupper($_GET['color']);
+    if (strlen($_GET['color']) === 3) {
+        $color =
+            strtoupper($_GET['color'][0] .
+                       $_GET['color'][0] .
+                       $_GET['color'][1] .
+                       $_GET['color'][1] .
+                       $_GET['color'][2] .
+                       $_GET['color'][2]);
+    }
+}
+list($colorRed, $colorGreen, $colorBlue) = sscanf($color, "%02x%02x%02x");
+
+/**
+ * Define the typeface settings.
+ */
+$fontFile = realpath(__DIR__) . DIRECTORY_SEPARATOR . 'RobotoMono-Regular.ttf';
+if ( ! is_readable($fontFile)) {
+    $fontFile = 'arial';
+}
+
+$fontSize = round(($imgWidth - 50) / 8);
+if ($fontSize <= 9) {
+    $fontSize = 9;
+}
+
+/**
+ * Generate the image.
+ */
+$image     = imagecreatetruecolor($imgWidth, $imgHeight);
+$colorFill = imagecolorallocate($image, $colorRed, $colorGreen, $colorBlue);
+$bgFill    = imagecolorallocate($image, $bgRed, $bgGreen, $bgBlue);
+imagefill($image, 0, 0, $bgFill);
+$textBox = imagettfbbox($fontSize, 0, $fontFile, $text);
+
+while ($textBox[4] >= $imgWidth) {
+    $fontSize -= round($fontSize / 2);
+    $textBox  = imagettfbbox($fontSize, 0, $fontFile, $text);
+    if ($fontSize <= 9) {
+        $fontSize = 9;
+        break;
+    }
+}
+$textWidth  = abs($textBox[4] - $textBox[0]);
+$textHeight = abs($textBox[5] - $textBox[1]);
+$textX      = ($imgWidth - $textWidth) / 2;
+$textY      = ($imgHeight + $textHeight) / 2;
+imagettftext($image, $fontSize, 0, $textX, $textY, $colorFill, $fontFile, $text);
+
+/**
+ * Return the image and destroy it afterwards.
+ */
+switch ($type) {
+    case 'png':
+        header('Content-Type: image/png');
+        imagepng($image, null, 9);
+        break;
+    case 'gif':
+        header('Content-Type: image/gif');
+        imagegif($image);
+        break;
+    case 'jpg':
+    case 'jpeg':
+        header('Content-Type: image/jpeg');
+        imagejpeg($image);
+        break;
+}
+imagedestroy($image);
